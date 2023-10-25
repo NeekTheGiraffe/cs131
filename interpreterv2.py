df8556a3c832b2f6715b130816d4380bb2dfdc71
Nick Pavlosky <44681231+NeekTheGiraffe@users.noreply.github.com>
2023-10-24 17:06:03 -0700

    P2: Implement return, if and while statements

--- interpreterv2.py
+++ interpreterv2.py
@@ -1,6 +1,6 @@
 from intbase import InterpreterBase, ErrorType
 from brewparse import parse_program
-import copy
+import copy, sys
 
 BINARY_OPERATORS = set(['+', '-', '*', '/', '==', '<', '<=', '>', '>=', '!=', '&&', '||'])
 UNARY_OPERATORS = set(['neg', '!'])
@@ -26,10 +26,18 @@ class TypedValue:
     def __init__(self, type: str, value: int | str | bool | None):
         self.type = type
         self.value = value
+    
+    def __repr__(self):
+        return f"({self.type} {self.value})"
 
 class Interpreter(InterpreterBase):
     def __init__(self, console_output=True, inp=None, trace_output=False):
         super().__init__(console_output, inp)
+        self.trace_output = trace_output
+
+    def print_if_trace(self, *args, **kwargs):
+        if self.trace_output:
+            print(*args, **kwargs, file=sys.stderr)
 
     def run(self, program):
         ast = parse_program(program)
@@ -49,18 +57,30 @@ class Interpreter(InterpreterBase):
                 ErrorType.NAME_ERROR,
                 "No main() function was found",
             )
-        self.run_func(main_func_node)
+        self.run_statements(main_func_node.dict['statements'])
 
-    def run_func(self, func_node):
-        for statement_node in func_node.dict['statements']:
-            self.run_statement(statement_node)
+    def run_statements(self, statement_list):
+        for statement_node in statement_list:
+            return_val = self.run_statement(statement_node)
+            if statement_node.elem_type == 'return' or (statement_node.elem_type in ['if', 'while'] and return_val is not None):
+                return return_val
+        return None
 
     def run_statement(self, statement_node):
         match statement_node.elem_type:
             case '=':
                 self.do_assignment(statement_node)
+                return None
             case 'fcall':
-                self.do_func_call(statement_node)
+                return self.do_func_call(statement_node)
+            case 'if':
+                return self.do_if_statement(statement_node)
+            case 'while':
+                return self.do_while_statement(statement_node)
+            case 'return':
+                if statement_node.dict['expression'] is None:
+                    return TypedValue('nil', None)
+                return copy.deepcopy(self.evaluate_expression(statement_node.dict['expression']))
 
     def is_variable_defined(self, varname):
         return varname in self.variables and len(self.variables[varname]) > 0
@@ -91,22 +111,64 @@ class Interpreter(InterpreterBase):
                 f"Function {func_name} that takes {len(args)} parameters has not been defined",
             )
         
-        # TODO: Return values
         func_decl_node = self.functions[func_name, len(args)]
         arg_names = [arg_node.dict['name'] for arg_node in func_decl_node.dict['args']]
         self.scopes.append(set(arg_names))
         for arg_name, value in zip(arg_names, args):
             if not self.is_variable_defined(arg_name):
-                self.variables[arg_name] = [copy.copy(value)]
+                self.variables[arg_name] = [copy.deepcopy(value)]
             else:
-                self.variables[arg_name].append(copy.copy(value))
+                self.variables[arg_name].append(copy.deepcopy(value))
 
-        self.run_func(func_decl_node)
+        return_val = self.run_statements(func_decl_node.dict['statements'])
 
         for varname in self.scopes[-1]:
             self.variables[varname].pop()
         self.scopes.pop()
-     
+        
+        if return_val is None:
+            return TypedValue('nil', None)
+        return return_val
+    
+    def do_if_statement(self, if_statement_node):
+        condition = self.evaluate_expression(if_statement_node.dict['condition'])
+        if condition.type != 'bool':
+            super().error(
+                ErrorType.TYPE_ERROR,
+                f"Expected bool inside 'if' condition but got {condition}"
+            )
+        statements = if_statement_node.dict['statements'] if condition.value else if_statement_node.dict['else_statements']
+        if statements is None:
+            return None
+        self.scopes.append(set())
+        return_val = self.run_statements(statements)
+        for var_name in self.scopes[-1]:
+            self.variables[var_name].pop()
+        self.scopes.pop()
+        return return_val
+
+    def do_while_statement(self, while_statement_node):
+        self.scopes.append(set())
+
+        return_val = None
+        while True:
+            condition = self.evaluate_expression(while_statement_node.dict['condition'])
+            if condition.type != 'bool':
+                super().error(
+                    ErrorType.TYPE_ERROR,
+                    f"Expected bool inside 'while' condition but got {condition}"
+                )
+            if not condition.value:
+                break
+            return_val = self.run_statements(while_statement_node.dict['statements'])
+            if return_val is not None:
+                break
+        
+        for var_name in self.scopes[-1]:
+            self.variables[var_name].pop()
+        self.scopes.pop()
+        return return_val
+
     def evaluate_expression(self, expression_node) -> TypedValue:
         if expression_node.elem_type in OPERATORS:
             return self.evaluate_operation(expression_node)
@@ -191,6 +253,8 @@ class Interpreter(InterpreterBase):
         for x in args:
             if x.type == 'bool':
                 output_strs.append("true" if x.value else "false")
+            elif x.type == 'nil':
+                output_strs.append("nil")
             else:
                 output_strs.append(str(x.value))
         super().output("".join(output_strs))
