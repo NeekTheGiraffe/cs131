31ebe6515d2dc78bc72ed52b1ac474c916efc452
Nick Pavlosky <44681231+NeekTheGiraffe@users.noreply.github.com>
2023-12-14 17:08:40 -0800

    Fix test.py

--- test.py
+++ test.py
@@ -2,15 +2,16 @@
 import subprocess, sys, shutil
 
 def main():
-    if len(sys.argv):
+    if len(sys.argv) < 1:
         print("Must provide a version number between 1-4")
         exit(1)
     version = sys.argv[1]
-    if version < 1 or version > 4:
+    version_num = int(version)
+    if version_num < 1 or version_num > 4:
         print("Must provide a version number between 1-4")
         exit(1)
     shutil.copy(f'interpreterv{version}.py', f'autograder/interpreterv{version}.py')
-    subprocess.run([sys.executable, 'tester.py', version], cwd='autograder')
+    subprocess.run([sys.executable, 'tester.py', str(version)], cwd='autograder')
 
 if __name__ == '__main__':
     main()
